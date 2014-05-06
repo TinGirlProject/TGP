48ababcf85e5f24c673a03e46f00575eeec3815e
Galen Manuel <galen.manuel@gmail.com>
2014-05-06 12:00:44 -0400

    Timer class added

--- TGP/Assets/Scripts/GameManager.cs
+++ TGP/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,9 +8,14 @@ public class GameManager : MonoBehaviour
     public Transform playerSpawn;
 	private GameObject currentPlayer;
     private CameraScrolling cam;
+	public static List<Timer> listOfTimers;
+	private Timer testTimer;
 	
 	void Start() 
     {
+		listOfTimers = new List<Timer>();
+		testTimer = new Timer(5.0f);
+		listOfTimers.Add(testTimer);
 		cam = GetComponent<CameraScrolling>();
         SpawnPlayer(playerSpawn.position);
 
@@ -25,12 +31,37 @@ public class GameManager : MonoBehaviour
 
 	private void Update() 
     {
+		for (int cnt = 0; cnt < listOfTimers.Count; ++cnt)
+		{
+			// if (gamestate != gamestate.pause)
+			listOfTimers[cnt].UpdateTimer();
+		}
+
 		if (!currentPlayer) 
         {
 			if (Input.GetButtonDown("Respawn")) 
             {
                 SpawnPlayer(playerSpawn.position);
 			}
+		}
+
+		if (Input.GetKeyUp(KeyCode.I))
+		{
+			if (!testTimer.IsTimerActive && !testTimer.IsTimeComplete)
+				testTimer.StartTimer();
+			else if (testTimer.IsTimeComplete)
+			{
+				testTimer.ResetTimer();
+				testTimer.StartTimer();
+			}
+		}
+
+		if (Input.GetKeyUp(KeyCode.U))
+		{
+			if (Time.timeScale == 1)
+				Time.timeScale = 0.5f;
+			else
+				Time.timeScale = 1;
 		}
 	}
 }
---
+++ TGP/Assets/Scripts/Utility/Timer.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer 
+{
+	private bool m_timeComplete;
+	private bool m_timerActive;
+	private float m_curTime;
+	private float m_defaultTime;
+	
+	public Timer()
+	{
+		m_timeComplete = false;
+		m_timerActive = false;
+		m_defaultTime = 1.0f;
+		m_curTime = m_defaultTime;
+	}
+	
+	public Timer(float defaultTime)
+	{
+		m_timeComplete = false;
+		m_timerActive = false;
+		m_defaultTime = defaultTime;
+		m_curTime = m_defaultTime;
+	}
+	
+	public void UpdateTimer() 
+	{
+		if (m_timerActive)
+		{
+			if (m_curTime > 0)
+			{
+				m_curTime -= Time.deltaTime * Time.timeScale;
+				Debug.Log ("Timer time: " + m_curTime);
+			}
+			else
+			{
+				m_timeComplete = true;
+				m_timerActive = false;
+			}
+		}
+	}
+
+	public void StartTimer()
+	{
+		m_timerActive = true;
+	}
+
+	public void PauseTimer()
+	{
+		m_timerActive = false;
+	}
+
+	public void ResetTimer()
+	{
+		m_curTime = m_defaultTime;
+		m_timerActive = false;
+		m_timeComplete = false;
+	}
+
+	public bool IsTimeComplete
+	{
+		get { return m_timeComplete; }
+	}
+	
+	public bool IsTimerActive
+	{
+		get { return m_timerActive; }
+	}
+
+	public float DefaultTime
+	{
+		get { return m_defaultTime; }
+		set { m_defaultTime = value; }
+	}
+
+	public float CurrentTime
+	{
+		get { return m_curTime; }
+	}
+}
