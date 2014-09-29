00f86d0f10c0acfc5768e45c292867ce526d33ff
joshkay <josh.m.kay@gmail.com>
2014-09-28 22:41:49 -0400

    Sept. 28th - Test Work
    Started working on simple tests

---
+++ TGP/Assets/Scripts/Tests/SceneList.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneList : MonoBehaviour 
+{
+    public string[] scenes;
+
+    public int marginX, marginY;
+
+    public int x, y;
+    public int width, height;
+
+    void OnGUI()
+    {
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            if (GUI.Button(new Rect(x + marginX * i, y + marginY * i, width, height), scenes[i]))
+            {
+                Application.LoadLevel(scenes[i]);
+            }
+        }
+    }
+}
