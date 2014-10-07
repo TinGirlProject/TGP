6116d256736ca15999f0c34aaeb1e4fb940a7f44
joshkay <josh.m.kay@gmail.com>
2014-10-07 15:34:19 -0400

    Oct. 07 - More character work

--- TGP/Assets/Scripts/NewPlayer/Character.cs
+++ TGP/Assets/Scripts/NewPlayer/Character.cs
@@ -13,7 +13,9 @@ public class Character : MonoBehaviour
 
     public Vector2 m_moveAmount;
 
-	void Start () 
+    private bool m_jump;
+
+	void Start()
     {
         m_collisions = GetComponent<CharacterCollisions>();
         m_movement = GetComponent<Movement>();
@@ -23,34 +25,44 @@ public class Character : MonoBehaviour
         m_moveAmount = new Vector2();
     }
         
-	void Update () 
+	void FixedUpdate() 
     {
-        m_moveAmount.x = m_speed * m_direction;
-
+        // apply sideways movement
+        m_moveAmount.x = m_speed * m_direction * Time.deltaTime;
+        // apply gravity
         m_moveAmount.y -= m_movement._gravity * Time.deltaTime;
 
+        if (m_jump)
+        {
+            m_moveAmount.y += m_movement._jumpHeight * Time.deltaTime;
+            m_jump = false;
+        }
+
+        // check for collisions and get modified movement
         m_moveAmount = m_collisions.TestMove(m_moveAmount);
 
+        // move the character
         DoMove(m_moveAmount);
 	}
 
     void DoMove(Vector2 amount)
     {
-        Log.GREEN(amount.x);
         m_transform.position += new Vector3(amount.x, amount.y, 0);
     }
 
     void Move(Movement.Direction dir)
     {
-        Log.BLUE(dir);
         switch (dir)
         {
             case Movement.Direction.Left:
-                transform.eulerAngles = Vector3.zero;
+                if (_previousDirection == Direction.Right)
+                {
+                    transform.Rotate(0, -180, 0);
+                }
                 m_direction = -1;
                 break;
             case Movement.Direction.Right:
-                transform.eulerAngles = Vector3.up * 180;
+                transform.rotation = new Quaternion(0, 180, 0, 0);
                 m_direction = 1;
                 break;
             case Movement.Direction.NONE:
@@ -62,7 +74,9 @@ public class Character : MonoBehaviour
     void Jump()
     {
         if (m_collisions.isGrounded())
-            m_moveAmount.y += m_movement._jumpHeight;
+        {
+            m_jump = true;
+        }
     }
 
     void Run(bool run)
